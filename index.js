9b54b93f28d484965ac8b4dbff3ec852e8c8aef1
SorayaBoolean <sorayakhyabaniassl@gmail.com>
2022-07-05 17:28:26 +0200

    added const for first name and second name

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-const tripKm = parseInt (prompt ('Inserisci il numero di Km di viaggio'));
+const costoPerkm = 0.21;
+
+const FirstAndSecondName = (prompt ('Inserisci tuo Nome e Cognome')) 
+const quantitaKm = parseInt (prompt ('Inserisci il numero di Km di viaggio'));
 const eta = parseInt (prompt ('Inserisci la tua età'))
 
-const prezzoXkm = 0.21;
+
+
+
 
 
