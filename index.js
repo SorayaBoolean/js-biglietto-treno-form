2b6fab91257574426378d7d8caccdfbf09932e47
SorayaBoolean <sorayakhyabaniassl@gmail.com>
2022-07-05 18:13:26 +0200

    add: added and fixed js code for sconto and message

--- index.js
+++ index.js
@@ -8,21 +8,21 @@ let prezzo = costoPerkm * quantitaKm;
 let messaggio = '';
 
 if (eta < 18) {
-    prezzo -= (prezzo / 100 * 20) ;
-    messaggio ='Il prezzo del biglietto è scontato del 20% . Il prezzo finale è ' + prezzo;
+    prezzo -= prezzo / 100 * 20 ;
+    messaggio ='Il prezzo del biglietto è scontato del 20% . Il prezzo finale è ' + prezzo.toFixed(2) + 'euro';
 
 } else if (eta > 65) {
-    prezzo -= (prezzo / 100 * 40) ;
-    messaggio ='Il prezzo del biglietto è scontato del 40% . Il prezzo finale è ' + prezzo;
+    prezzo -= prezzo / 100 * 40 ;
+    messaggio ='Il prezzo del biglietto è scontato del 40% . Il prezzo finale è ' + prezzo.toFixed(2) + 'euro';
 } else {
-    messaggio = 'Il prezzo del biglietto è ' + prezzo;
+    messaggio = 'Il prezzo del biglietto è ' + prezzo.toFixed(2) + 'euro';
 }
 
 
 
 
 
-alert (messaggio)
+alert (messaggio);
 
 
 
