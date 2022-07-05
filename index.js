7d579ad4d54addbc6fd5af7fcbf68c4c3823bbfc
SorayaBoolean <sorayakhyabaniassl@gmail.com>
2022-07-05 16:57:36 +0200

    add: added isNan for kmTrip prompt

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 const tripKm = parseInt (prompt ('Inserisci il numero di Km di viaggio'));
+
+if (isNan(tripKm)) {
+    alert ('Valore inserito non valido');
+} else {
+    alert ('Hai inserito: ' + tripKm)
+}
